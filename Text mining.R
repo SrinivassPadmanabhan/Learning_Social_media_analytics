66775db463a2966011bcd12e7386a2a891a0e9a5
SRINIVASS P P <srinivassg8@gmail.com>
2021-01-26 10:24:54 +0530

    new file for youtube API

---
+++ Text mining.R
@@ -0,0 +1,7 @@
+library(tuber)
+app_id="144955269209-teo1seclp7gmpfdkqs4l9jd264k5qf29.apps.googleusercontent.com"
+app_secret="XqVEBFClRb7Q0vmUQkNDwbED"
+yt_oauth(app_id=app_id, app_secret=app_secret, token='')
+#to get the help
+help(tuber)
+video_stat = get_stats(video_id="y8HEZ-x4-_w")
